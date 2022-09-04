641fa842e8c59ae005d5e0b13811367a4f0fbcba
Josh <jsbusiness305@gmail.com>
2022-09-04 14:30:30 -0400

    Add project files.

---
+++ MyATMApp/ATMApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyATMApp
+{
+    public class ATMApp
+    {
+        static void Main(string[] args)
+        {
+
+            //clear the console
+            Console.Clear();
+            //set console title color
+            Console.ForegroundColor = ConsoleColor.White;
+            //add title to the console
+            Console.Title = "\n\n----------Welcome To My ATM App----------\n\n";
+            //prompt the user to insert their atm card
+            Console.WriteLine("Please insert your ATM card.\n");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate the physical ATM card, read the card number and validate it.");
+            
+                Console.WriteLine("\n\nPress Enter to continue...\n");
+            Console.ReadLine();
+        }
+    }
+}
